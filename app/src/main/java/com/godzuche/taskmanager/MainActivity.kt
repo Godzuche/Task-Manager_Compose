d460038ea0dbf6fbcb6530a42d7a36c922fb8192
Administrator <godxuche@gmail.com>
2022-05-04 22:53:32 +0100

    done solving the practice problem...

--- app/src/main/java/com/godzuche/taskmanager/MainActivity.kt
+++ app/src/main/java/com/godzuche/taskmanager/MainActivity.kt
@@ -3,13 +3,24 @@ package com.godzuche.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.godzuche.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +31,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    TaskManagerApp()
                 }
             }
         }
@@ -28,14 +39,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskManagerApp() {
+    TaskCompletedScreen(
+        imagePainter = painterResource(R.drawable.ic_task_completed),
+        message = stringResource(R.string.all_task_completed),
+        compliment = stringResource(R.string.nice_work),
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@Composable
+fun TaskCompletedScreen(
+    imagePainter: Painter,
+    message: String,
+    compliment: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Image(
+            painter = imagePainter,
+            contentDescription = null
+        )
+        Text(
+            text = message,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = compliment,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun TaskManagerPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        Surface {
+            TaskManagerApp()
+        }
     }
 }
